c8c44a3a2c7d8f0e0c95985239efb2b05a15701f
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-08-03 17:56:31 +0200

    changed hamburger button closing styles

--- scripts/hamburgerMenu.js
+++ scripts/hamburgerMenu.js
@@ -4,6 +4,7 @@ const links = document.querySelectorAll(".menu-link");
 
 const toggleMenu = () => {
   hamburgerButton.addEventListener("click", () => {
+    hamburgerButton.classList.toggle("toggle");
     menu.classList.toggle("active");
     links.forEach((link) =>
       link.addEventListener("click", () => {
